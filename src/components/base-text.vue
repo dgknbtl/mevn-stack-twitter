64d4bef74692eefa2de2c48a13eb0b955084de7b
Doğukan Batal <dogukanbatal@gmail.com>
2022-06-10 23:15:38 +0300

    Add base text component

---
+++ src/components/base-text.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+   name: 'BaseText',
+   props: {
+      tag: {
+         default: 'span',
+      },
+      size: {
+         default: 'medium',
+         validator: function (value) {
+            return ['large', 'medium', 'small'].indexOf(value) !== -1
+         },
+      },
+      weight: {
+         default: '400',
+         validator: function (value) {
+            return (
+               [
+                  'light',
+                  'regular',
+                  'medium',
+                  'semibold',
+                  'bold',
+                  'extrabold',
+                  'black',
+               ].indexOf(value) !== -1
+            )
+         },
+      },
+   },
+}
+</script>
+
+<template lang="pug">
+component(:is="tag" :class="[size, weight]")
+   slot
+</template>
+
+<style lang="postcss" scoped>
+.large {
+   font-size: var(--fs-lg);
+}
+.medium {
+   font-size: var(--fs-md);
+}
+.small {
+   font-size: var(--fs-sm);
+}
+
+.light {
+   font-weight: 300;
+}
+.regular {
+   font-weight: 400;
+}
+.medium {
+   font-weight: 500;
+}
+.semibold {
+   font-weight: 600;
+}
+.bold {
+   font-weight: 700;
+}
+.extrabold {
+   font-weight: 800;
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,22 @@
+<script>
+import BaseText from '@/components/base-text'
+
+export default {
+   name: 'HomeView',
+   components: {
+      BaseText,
+   },
+}
+</script>
+
+<template lang="pug">
+div
+   BaseText(tag="p" size="small") Hello
+   BaseText(class="primary" tag="p" size="large" weight="bold") Hello
+</template>
+
+<style lang="postcss" scoped>
+.primary {
+   color: rgb(var(--c-primary));
+}
+</style>
