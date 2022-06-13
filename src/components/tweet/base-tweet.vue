b621640e64cea1a0feeb4b0ae687c0ca9eef7d3f
Doğukan Batal <dogukanbatal@gmail.com>
2022-06-13 21:37:56 +0300

    Add base-tweet and new tweet components

---
+++ src/components/tweet/base-tweet.vue
@@ -0,0 +1,188 @@
+<script>
+import BaseAvatar from '@/components/base-avatar.vue'
+import BaseText from '@/components/base-text.vue'
+import InlineSvg from 'vue-inline-svg'
+
+export default {
+   name: 'BaseTweet',
+   components: {
+      BaseAvatar,
+      BaseText,
+      InlineSvg,
+   },
+   props: {
+      tweetData: {
+         type: Object,
+         required: true,
+      },
+   },
+   data() {},
+}
+</script>
+
+<template lang="pug">
+div.tweet
+   .tweet-avatar
+      BaseAvatar(size="large" :src="tweetData.avatar")
+   .tweet-content
+      .tweet-header
+         div            
+            RouterLink(:to="{ path: `/${tweetData.handle}` }" tag="a")
+               BaseText(class="tweet-author" weight="fw-bold" tag="a") {{tweetData.author}}
+            BaseText(class="tweet-handle") {{tweetData.handle}}
+            BaseText  •
+            RouterLink(:to="{ path: `/${tweetData.handle}/status/AGFGFB` }" tag="a")
+               BaseText(class="tweet-time") {{tweetData.creaatedAt}}
+         
+         .tweet-more: InlineSvg(:src="require('@/assets/icons/dot.svg')" width="20")
+      .tweet-content
+         BaseText(tag="p") {{tweetData.content}}
+         figure.tweet-image(v-if="tweetData.media")
+            img(:src="tweetData.media")
+         .tweet-actions
+            .tweet-action
+               .action-icon: InlineSvg(:src="require('@/assets/icons/comment.svg')" width="18")
+               BaseText(size="fs-small") {{tweetData.comment}}
+            .tweet-action
+               .action-icon: InlineSvg(:src="require('@/assets/icons/retweet.svg')" width="18")
+               BaseText(size="fs-small") {{tweetData.retweet}}
+            .tweet-action
+               .action-icon: InlineSvg(:src="require('@/assets/icons/like.svg')" width="18")
+               BaseText(size="fs-small") {{tweetData.likes}}
+            .tweet-action
+               .action-icon: InlineSvg(:src="require('@/assets/icons/share.svg')" width="16")
+               
+
+</template>
+
+<style lang="postcss" scoped>
+.tweet {
+   padding: var(--gap-1);
+   display: flex;
+   align-items: stretch;
+   border-bottom: 1px solid rgb(var(--c-light-2));
+   &:hover {
+      cursor: pointer;
+      background-color: rgba(var(--c-light-2), 0.7);
+   }
+   &-avatar {
+      margin-right: 12px;
+   }
+   &-header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      a:hover {
+         text-decoration: underline;
+      }
+   }
+   &-more {
+      width: 32px;
+      height: 32px;
+      border-radius: 999px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      &:hover {
+         svg {
+            fill: rgb(var(--c-primary));
+         }
+         background-color: rgba(var(--c-primary), 0.1);
+      }
+   }
+   span {
+      color: rgb(var(--c-gray));
+   }
+   &-handle {
+      padding-left: 6px;
+      padding-right: 6px;
+   }
+   &-time {
+      padding-left: 4px;
+   }
+   &-content {
+      line-height: 20px;
+   }
+   &-image {
+      border-radius: 15px;
+      border: 1px solid rgb(var(--c-light));
+      margin-top: var(--gap-1);
+      img {
+         border-radius: inherit;
+      }
+   }
+   &-actions {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      max-width: 425px;
+      margin-top: 12px;
+      .tweet-action {
+         display: flex;
+         align-items: center;
+         margin-left: -5px;
+         .action-icon {
+            width: 30px;
+            height: 30px;
+            border-radius: 999px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+         }
+         span {
+            padding-left: 4px;
+         }
+         svg {
+            fill: rgb(var(--c-gray));
+         }
+         &:hover {
+            .action-icon {
+               background-color: rgba(var(--c-primary), 0.1);
+            }
+            svg {
+               fill: rgb(var(--c-primary));
+            }
+         }
+         &:nth-child(1) {
+            &:hover {
+               span {
+                  color: rgb(var(--c-primary));
+               }
+               .action-icon {
+                  background-color: rgba(var(--c-primary), 0.1);
+               }
+               svg {
+                  fill: rgb(var(--c-primary));
+               }
+            }
+         }
+         &:nth-child(2) {
+            &:hover {
+               span {
+                  color: rgb(var(--c-green));
+               }
+               .action-icon {
+                  background-color: rgba(var(--c-green), 0.1);
+               }
+               svg {
+                  fill: rgb(var(--c-green));
+               }
+            }
+         }
+         &:nth-child(3) {
+            &:hover {
+               span {
+                  color: rgb(var(--c-red));
+               }
+               .action-icon {
+                  background-color: rgba(var(--c-red), 0.1);
+               }
+               svg {
+                  fill: rgb(var(--c-red));
+               }
+            }
+         }
+      }
+   }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,8 @@
 <script>
 import BaseHeading from '@/components/base-heading'
 import BaseText from '@/components/base-text'
-import NewTweet from '@/components/new-tweet'
+import NewTweet from '@/components/tweet/new-tweet'
+import BaseTweet from '@/components/tweet/base-tweet'
 
 export default {
    name: 'HomeView',
@@ -9,6 +10,62 @@ export default {
       BaseHeading,
       BaseText,
       NewTweet,
+      BaseTweet,
+   },
+   data() {
+      return {
+         exampleData: [
+            {
+               avatar:
+                  'https://pbs.twimg.com/profile_images/1534871110856081410/ihb2cTGs_400x400.jpg',
+               author: 'Doğukan Batal',
+               handle: 'dogukanbatal',
+               creaatedAt: '11h',
+               content:
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ',
+               likes: 5,
+               retweet: 100,
+               comment: 1,
+               media: 'https://pbs.twimg.com/media/FVIJd2oXwAE6ic_?format=jpg&name=medium',
+            },
+            {
+               avatar: undefined,
+               author: 'The Economist ',
+               handle: 'TheEconomist',
+               creaatedAt: '1d',
+               content:
+                  'At least we’re safe here because Bitcoin has never dipped beneath a previous cycle’s all time high before! Right?',
+               likes: 50,
+               retweet: 5,
+               comment: 3,
+               media: null,
+            },
+            {
+               avatar: 'https://pbs.twimg.com/profile_images/72647502/tyler_400x400.jpg',
+               author: 'Zero Hedge',
+               handle: 'zerohedge',
+               creaatedAt: '11h',
+               content:
+                  'We Are Trading Like It`s A Race Which Central Bank Can Crash Their Economy The Fastest  ',
+               likes: 55,
+               retweet: 5,
+               comment: 10,
+               media: 'https://pbs.twimg.com/card_img/1536414470658052102/j1mc7Fmo?format=jpg&name=small',
+            },
+            {
+               avatar: undefined,
+               author: 'The Economist ',
+               handle: 'TheEconomist',
+               creaatedAt: '1d',
+               content:
+                  'At least we’re safe here because Bitcoin has never dipped beneath a previous cycle’s all time high before! Right?',
+               likes: 50,
+               retweet: 5,
+               comment: 3,
+               media: null,
+            },
+         ],
+      }
    },
    methods: {
       someEvent() {
@@ -22,6 +79,8 @@ export default {
 div
    BaseHeading(title="Latest Tweets" icon="star" @icon-action="someEvent")
    NewTweet
+   div(v-for="(tweet,index) in exampleData" key="index")
+      BaseTweet(:tweetData="tweet")
 </template>
 
 <style lang="postcss" scoped></style>
