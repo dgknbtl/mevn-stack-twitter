e06f4159afbe60597f9b2ceb7eadaccfd2c332a3
Doğukan Batal <dogukanbatal@gmail.com>
2022-06-13 14:01:44 +0300

    Add base-avatar component

---
+++ src/components/base-avatar.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+   name: 'BaseAvatar',
+   props: {
+      src: {
+         type: String,
+         default: require('@/assets/images/default-avatar.png'),
+      },
+      size: {
+         default: 'medium',
+         validator: function (value) {
+            return ['small', 'medium', 'large', 'xlarge'].indexOf(value) !== -1
+         },
+      },
+   },
+}
+</script>
+
+<template lang="pug">
+figure.avatar
+   img.avatar-img(:src="src" :class="size")
+</template>
+
+<style lang="postcss" scoped>
+.avatar {
+   border-radius: 999px;
+   img {
+      border-radius: inherit;
+   }
+}
+.small {
+   width: var(--avatar-sm);
+   height: var(--avatar-sm);
+}
+.medium {
+   width: var(--avatar-md);
+   height: var(--avatar-md);
+}
+.large {
+   width: var(--avatar-lg);
+   height: var(--avatar-lg);
+}
+.xlarge {
+   width: var(--avatar-xlg);
+   height: var(--avatar-xlg);
+}
+</style>
