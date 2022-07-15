9bc9327ec2345d177d9c4ea4022cf06cdff09b71
Doğukan Batal <dogukanbatal@gmail.com>
2022-07-16 02:16:08 +0300

    Update fetching user data

--- frontend/src/views/home/index.vue
+++ frontend/src/views/home/index.vue
@@ -2,7 +2,7 @@
 import BaseHeading from '@/components/base-heading'
 import BaseText from '@/components/base-text'
 import NewTweet from '@/components/tweet/new-tweet'
-import BaseTweet from '@/components/tweet/base-tweet'
+// import BaseTweet from '@/components/tweet/base-tweet'
 
 export default {
    name: 'HomeView',
@@ -10,63 +10,9 @@ export default {
       BaseHeading,
       BaseText,
       NewTweet,
-      BaseTweet,
+      // BaseTweet,
    },
-   data() {
-      return {
-         exampleData: [
-            {
-               avatar:
-                  'https://pbs.twimg.com/profile_images/1544667412120879104/Z4vKdOuy_400x400.jpg',
-               author: 'Doğukan Batal',
-               handle: 'dogukanbatal',
-               creaatedAt: '11h',
-               content:
-                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ',
-               likes: 5,
-               retweet: 100,
-               comment: 1,
-               media: 'https://pbs.twimg.com/media/FVIJd2oXwAE6ic_?format=jpg&name=medium',
-            },
-            {
-               avatar: undefined,
-               author: 'The Economist ',
-               handle: 'TheEconomist',
-               creaatedAt: '1d',
-               content:
-                  'At least we’re safe here because Bitcoin has never dipped beneath a previous cycle’s all time high before! Right?',
-               likes: 50,
-               retweet: 5,
-               comment: 3,
-               media: null,
-            },
-            {
-               avatar: 'https://pbs.twimg.com/profile_images/72647502/tyler_400x400.jpg',
-               author: 'Zero Hedge',
-               handle: 'zerohedge',
-               creaatedAt: '11h',
-               content:
-                  'We Are Trading Like It`s A Race Which Central Bank Can Crash Their Economy The Fastest  ',
-               likes: 55,
-               retweet: 5,
-               comment: 10,
-               media: 'https://pbs.twimg.com/card_img/1536414470658052102/j1mc7Fmo?format=jpg&name=small',
-            },
-            {
-               avatar: undefined,
-               author: 'The Economist ',
-               handle: 'TheEconomist',
-               creaatedAt: '1d',
-               content:
-                  'At least we’re safe here because Bitcoin has never dipped beneath a previous cycle’s all time high before! Right?',
-               likes: 50,
-               retweet: 5,
-               comment: 3,
-               media: null,
-            },
-         ],
-      }
-   },
+   data() {},
    methods: {
       someEvent() {
          alert()
@@ -81,8 +27,8 @@ div
    
    NewTweet
    
-   div(v-for="(tweet,index) in exampleData" key="index")
-      BaseTweet(:tweetData="tweet")
+   //- div(v-for="(tweet,index) in exampleData" key="index")
+   //-    BaseTweet(:tweetData="tweet")
 </template>
 
 <style lang="postcss" scoped></style>
--- frontend/src/views/profile/tweets.vue
+++ frontend/src/views/profile/tweets.vue
@@ -1,21 +1,32 @@
 <script>
 import BaseTweet from '@/components/tweet/base-tweet.vue'
-import {mapState} from 'vuex'
+import BaseText from '@/components/base-text.vue'
 
 export default {
    name: 'TweetsView',
    components: {
       BaseTweet,
+      BaseText,
    },
    computed: {
-      ...mapState(['user']),
+      tweets() {
+         return this.$store.state.user.tweets
+      },
    },
 }
 </script>
 
 <template lang="pug">
-div(v-for="(tweet,index) in user.tweets" key="index")
+BaseText(size="fs-large" weight="fw-bold" class="text" v-if="!tweets.length") You don’t have any tweets yet
+div(v-for="(tweet,index) in tweets" key="index" v-else)
    BaseTweet(:tweet="tweet")
 </template>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+.text {
+   padding: 30px;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+}
+</style>
