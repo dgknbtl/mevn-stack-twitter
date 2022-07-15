789391c035988bc421953d4e6a5dc5b4b39fe13c
Doğukan Batal <dogukanbatal@gmail.com>
2022-07-16 00:54:20 +0300

    Add base-loader component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
 import InlineSvg from 'vue-inline-svg'
 import DefaultLayout from '@/layouts/default.vue'
 import AuthLayout from '@/layouts/auth.vue'
-import {mapActions, mapState} from 'vuex'
+import BaseLoader from '@/components/base-loader.vue'
 
 export default {
    name: 'App',
@@ -10,14 +10,9 @@ export default {
       InlineSvg,
       DefaultLayout,
       AuthLayout,
-   },
-   data() {
-      return {
-         loading: true,
-      }
+      BaseLoader,
    },
    computed: {
-      ...mapState(['isLoggedIn']),
       layout() {
          return this.$route.meta.layout || 'DefaultLayout'
       },
@@ -25,56 +20,12 @@ export default {
          return [this.$route.name]
       },
    },
-   methods: {
-      ...mapActions(['initialize']),
-   },
-   async mounted() {
-      setTimeout(() => {
-         this.loading = false
-      }, 1000)
-      if (!this.isLoggedIn) return
-      await this.initialize()
-   },
 }
 </script>
 
 <template lang="pug">
-.page-loader(v-show="loading")
-   .page-loader-logo: InlineSvg(:src="require('@/assets/icons/twitter.svg')")
-
-component(:is="layout" :selectedKeys="selectedKeys" v-if="!loading")
-   RouterView
-   //- router-view
+div
+   BaseLoader
+   component(:is="layout" :selectedKeys="selectedKeys" v-if="!loading")
+      RouterView
 </template>
-
-<style lang="postcss" scoped>
-.page-loader {
-   position: fixed;
-   top: 0;
-   left: 0;
-   width: 100%;
-   height: 100%;
-   background-color: #fff;
-   display: flex;
-   align-items: center;
-   justify-content: center;
-   z-index: 9999;
-   &-logo {
-      transform: scale(1);
-      animation: logo 1500ms;
-      svg {
-         width: 70px;
-         height: 70px;
-         fill: rgb(var(--c-primary));
-      }
-      @keyframes logo {
-         from {
-            transform: scale(0.7);
-         }
-         to {
-            transform: scale(1);
-         }
-      }
-   }
-}
-</style>
---
+++ frontend/src/components/base-loader.vue
@@ -0,0 +1,57 @@
+<script>
+import InlineSvg from 'vue-inline-svg'
+
+export default {
+   name: 'BaseLoader',
+   components: {
+      InlineSvg,
+   },
+   data() {
+      return {
+         loading: true,
+      }
+   },
+   mounted() {
+      setTimeout(() => {
+         this.loading = false
+      }, 1000)
+   },
+}
+</script>
+
+<template lang="pug">
+.page-loader(v-show="loading")
+   .page-loader-logo: InlineSvg(:src="require('@/assets/icons/twitter.svg')")
+</template>
+
+<style lang="postcss" scoped>
+.page-loader {
+   position: fixed;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100%;
+   background-color: #fff;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   z-index: 9999;
+   &-logo {
+      transform: scale(1);
+      animation: logo 1500ms;
+      svg {
+         width: 70px;
+         height: 70px;
+         fill: rgb(var(--c-primary));
+      }
+      @keyframes logo {
+         from {
+            transform: scale(0.7);
+         }
+         to {
+            transform: scale(1);
+         }
+      }
+   }
+}
+</style>
