8c52e61dba81f8c75c47805b223a05a654548620
Dogukan Batal <dogukanbatal@gmail.com>
2024-01-23 20:32:02 +0300

    Update dev env

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,8 @@
 const {defineConfig} = require('@vue/cli-service')
+
 module.exports = defineConfig({
    transpileDependencies: true,
    devServer: {
-      public: '0.0.0.0:8080',
       allowedHosts: 'all',
       client: {
          webSocketURL: 'auto://0.0.0.0:0/ws',
